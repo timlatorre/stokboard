bdbe569a3eea742638ebc26ee70bf7876469eb16
Tim LaTorre <timlatorre@gmail.com>
2012-12-07 10:15:39 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stokboard::Application.config.secret_token = '6d507e9c785e60e98301835831319fd90ab3956d0153605cd13260032e07539ade7d235530f816c01d0c5d525c7f589d1d41621a74d4baf8181361113ccde640'
